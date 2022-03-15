1f601e27be17a36727c36af427572dab17daf707
AmigasALO <100226279+RodriNumber1@users.noreply.github.com>
2022-03-15 12:44:11 +0100

    Update Ejercicio2.java

--- src/Ejercicio2.java
+++ src/Ejercicio2.java
@@ -1,3 +1,14 @@
-public class Ejercicio2 {
+import java.util.Scanner;
+import java.util.regex.*;
 
+public class Ejercicio2 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        String text = s.nextLine();
+        Pattern p = Pattern.compile("\\b([E][\\- ]?)?\\d{4}[\\-| ]?[A-Z]{3}\\b");
+        Matcher m = p.matcher((text));
+        while(m.find()){
+            System.out.println(m.group());
+        }
+    }
 }
