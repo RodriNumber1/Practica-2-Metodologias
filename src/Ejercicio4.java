5699b524263c2956a8649de512db3d68c77e3ec8
RubenJuzgado <83213976+RubenJuzgado@users.noreply.github.com>
2022-03-15 12:20:05 +0100

    Ejercicio4

--- src/Ejercicio4.java
+++ src/Ejercicio4.java
@@ -4,11 +4,11 @@ import java.util.regex.*;
 public class Ejercicio4 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String text = scanner.next();
-        Pattern p = Pattern.compile("\\b[\\d]{4}\\b");
+        String text = scanner.nextLine();
+        Pattern p = Pattern.compile("((?:Calle)|(?:C\\/)) ([A-Z]\\w+),? *(?:[Nn]º? ?)?((?:\\d)+), *(\\d{5})");
         Matcher m = p.matcher((text));
         while(m.find()){
-            System.out.println(m.group());
+            System.out.println(m.group(4)+"-"+m.group(2)+"-"+m.group(3));
         }
     }
 }
