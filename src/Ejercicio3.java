6409fd9ae8b8f0a316ce214197fd74217125bda4
RubenJuzgado <83213976+RubenJuzgado@users.noreply.github.com>
2022-03-16 18:32:19 +0100

    Ejercicio3

--- src/Ejercicio3.java
+++ src/Ejercicio3.java
@@ -5,12 +5,12 @@ public class Ejercicio3 {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
         String text = s.nextLine();
-        Pattern p = Pattern.compile("\\b\\w.(\\w{2,}).(\\d{4})@alumnos\\.urjc\\.es\\b|\\b(\\w+).(\\w+)@urjc\\.es\\b");
+        Pattern p = Pattern.compile("\\b\\w\\.(\\w{2,})\\.(\\d{4})@alumnos\\.urjc\\.es\\b|\\b(\\w+)\\.(\\w+)@urjc\\.es\\b");
         Matcher m = p.matcher((text));
         while(m.find()){
-            if (!(m.group(1).isEmpty() && m.group(2).isEmpty())){
+            if (!(m.group(1) == null && m.group(2) == null)){
                 System.out.println("alumno "+ m.group(1) + " matriculado en " + m.group(2));
-            } else if (!(m.group(3).isEmpty() && m.group(4).isEmpty())){
+            } else {
                 System.out.println("profesor " + m.group(3) + " " + m.group(4));
             }
         }
--- src/Ejercicio4.java
+++ src/Ejercicio4.java
@@ -5,10 +5,10 @@ public class Ejercicio4 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String text = scanner.nextLine();
-        Pattern p = Pattern.compile("((?:Calle)|(?:C\\/)) ([A-Z]\\w+),? *(?:[Nn]º? ?)?((?:\\d)+), *(\\d{5})");
+        Pattern p = Pattern.compile("((?:Calle)|(?:C\\/)) ([A-Z]\\w+),? +(?:[Nn]º? ?)?((?:\\d)+), *(\\d{5})");
         Matcher m = p.matcher((text));
         while(m.find()){
-            System.out.println(m.group(4)+"-"+m.group(2)+"-"+m.group(3));
+            System.out.println(m.group(4) + "-" + m.group(2) + "-" + m.group(3));
         }
     }
 }
