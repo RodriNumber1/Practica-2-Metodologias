c513ac997b5b364f437be0a51fab422af64768fd
Oscar del Rio <91894632+Oscarmanz@users.noreply.github.com>
2022-03-17 17:48:55 +0100

    Update Ejercicio2.java

--- src/Ejercicio2.java
+++ src/Ejercicio2.java
@@ -3,12 +3,16 @@ import java.util.regex.*;
 
 public class Ejercicio2 {
     public static void main(String[] args) {
-        Scanner s = new Scanner(System.in);
-        String text = s.nextLine();
-        Pattern p = Pattern.compile("\\b([E][\\- ]?)?\\d{4}[\\-| ]?[A-Z]{3}\\b");
-        Matcher m = p.matcher((text));
+        Scanner scanner = new Scanner(System.in);
+        String text = scanner.nextLine();
+        Pattern p = Pattern.compile("\\b(E[- ]?)?([\\d]{4}[- ]?[A-Z]{3})\\b");
+        Matcher m = p.matcher(text);
         while(m.find()){
-            System.out.println(m.group());
+            if (m.group(1) != null){
+                System.out.println(m.group(1) + m.group(2));
+            } else {
+                System.out.println(m.group(2));
+            }
         }
     }
 }
