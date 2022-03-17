e632fe1450c49d57c44c528a146ae7b9b8209245
RubenJuzgado <83213976+RubenJuzgado@users.noreply.github.com>
2022-03-17 17:37:17 +0100

    Update Ejercicio4.java

--- src/Ejercicio4.java
+++ src/Ejercicio4.java
@@ -5,7 +5,7 @@ public class Ejercicio4 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String text = scanner.nextLine();
-        Pattern p = Pattern.compile("((?:Calle)|(?:C\\/)) ([A-Z]\\w+),? +(?:[Nn]º? ?)?((?:\\d)+), *(\\d{5})");
+        Pattern p = Pattern.compile("\\b((?:Calle)|(?:C\\/)) ([A-Z][a-zñáéíóú]+),? +(?:[Nn]º? ?)?((?:\\d)+), *(\\d{5})\\b");
         Matcher m = p.matcher((text));
         while(m.find()){
             System.out.println(m.group(4) + "-" + m.group(2) + "-" + m.group(3));
