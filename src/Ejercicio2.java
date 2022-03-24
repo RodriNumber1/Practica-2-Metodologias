5ca3ef93698efa4a5116d8af54c86bb52b76b0f9
Ferno13 <91548963+Ferno13@users.noreply.github.com>
2022-03-24 18:27:52 +0100

    Update Ejercicio2.java
    Corchetes innecesarios

--- src/Ejercicio2.java
+++ src/Ejercicio2.java
@@ -5,7 +5,7 @@ public class Ejercicio2 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String text = scanner.nextLine();
-        Pattern p = Pattern.compile("\\b(E[- ]?)?([\\d]{4}[- ]?[A-Z]{3})\\b");
+        Pattern p = Pattern.compile("\\b(E[- ]?)?(\\d{4}[- ]?[A-Z]{3})\\b");
         Matcher m = p.matcher(text);
         while(m.find()){
             if (m.group(1) != null){
