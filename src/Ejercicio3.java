abb15fed01786f957c635428e2f49ba65e9ee162
Ferno13 <91548963+Ferno13@users.noreply.github.com>
2022-03-24 18:33:03 +0100

    Update Ejercicio3.java
    No hacían falta los \\b, incluidos en \\w

--- src/Ejercicio3.java
+++ src/Ejercicio3.java
@@ -5,7 +5,7 @@ public class Ejercicio3 {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
         String text = s.nextLine();
-        Pattern p = Pattern.compile("\\b\\w\\.(\\w{2,})\\.(\\d{4})@alumnos\\.urjc\\.es\\b|\\b(\\w+)\\.(\\w+)@urjc\\.es\\b");
+        Pattern p = Pattern.compile("\\w\\.(\\w{2,})\\.(\\d{4})@alumnos\\.urjc\\.es|(\\w+)\\.(\\w+)@urjc\\.es");
         Matcher m = p.matcher((text));
         while(m.find()){
             if (!(m.group(1) == null && m.group(2) == null)){
