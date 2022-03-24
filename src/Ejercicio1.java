1cef7b6a27c705f6fd9fbc493fc34d4e71f26d53
Ferno13 <91548963+Ferno13@users.noreply.github.com>
2022-03-24 18:22:37 +0100

    Update Ejercicio1.java
    No eran necesarios los corchetes

--- src/Ejercicio1.java
+++ src/Ejercicio1.java
@@ -5,7 +5,7 @@ public class Ejercicio1 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String text = scanner.nextLine();
-        Pattern p = Pattern.compile("\\b[\\d]{4}\\b");
+        Pattern p = Pattern.compile("\\b\\d{4}\\b");
         Matcher m = p.matcher((text));
         while(m.find()){
             System.out.println(m.group());
